50de661e28e1fc6f94b9819b0771bb3eb65fff3f
Benjamin Venditti <venditbl@clarkson.edu>
2020-10-21 21:05:58 -0400

    EnemyAttack
    BasicEnemy1 Will now damage a player if it collides with it, then it will rest for a couple seconds before moving again. Player has health as well.

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float HP = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Hit(float damage) {
+        HP -= damage;
+
+    }
+}
